dd25c8fd4d0a81950fa34ccd49a0578c2995cca5
Rane M Kaseman <raykman@comcast.net>
2018-08-17 13:41:00 -0600

    1340

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,32 +1,28 @@
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBmeleQU0z3P1C6sbEnlStaE7msi-2JzxM",
-    authDomain: "trains-35781.firebaseapp.com",
-    databaseURL: "https://trains-35781.firebaseio.com",
-    projectId: "trains-35781",
-    storageBucket: "trains-35781.appspot.com",
-    messagingSenderId: "755160834451"
-};
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyBmeleQU0z3P1C6sbEnlStaE7msi-2JzxM",
+//     authDomain: "trains-35781.firebaseapp.com",
+//     databaseURL: "https://trains-35781.firebaseio.com",
+//     projectId: "trains-35781",
+//     storageBucket: "trains-35781.appspot.com",
+//     messagingSenderId: "755160834451"
+// };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
 var name = "";
-var destination = "";
-var frequency = 0;
-var departure = "";
-var arrival = "";
+var guestEmail = "";
+var subject = "";
 
 $("#sendForm").on("click", function(event) {
     event.preventDefault();
-    name = $("#nameEnter").val().trim();
-    destination = $("#destEnter").val().trim();
-    frequency = $("#freqEnter").val().trim();
-    departure = $("#departEnter").val().trim();
+    name = $("#name").val().trim();
+    guestEmail = $("#guestEmail").val().trim();
+    message = $("#subject").val().trim();
     console.log("name : " + name);
-    console.log("destination : " + destination);
-    console.log("frequency : " + frequency);
-    console.log("departure : " + departure);
+    console.log("guestEmail : " + guestEmail);
+    console.log("message : " + message);
 
     // push back time 1 year
     var arrivalConvert = moment(departure, "HH:mm").subtract(1, "years");
