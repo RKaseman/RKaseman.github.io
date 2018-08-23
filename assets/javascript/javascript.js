748ec3db9ac24366e331a1e62fe366ac30b2c6d6
Rane M Kaseman <raykman@comcast.net>
2018-08-23 17:17:18 -0600

    1717

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,65 +13,25 @@ firebase.initializeApp(config);
 
 var name = "";
 var guestEmail = "";
-var subject = "";
+var message = "";
 
 $("#sendForm").on("click", function(event) {
     event.preventDefault();
     name = $("#name").val().trim();
     guestEmail = $("#guestEmail").val().trim();
-    message = $("#subject").val().trim();
+    message = $("#message").val().trim();
     console.log("name : " + name);
     console.log("guestEmail : " + guestEmail);
     console.log("message : " + message);
 
-    // push back time 1 year
-    var arrivalConvert = moment(departure, "HH:mm").subtract(1, "years");
-    console.log("arrivalConvert = " + moment(departure, "HH:mm").subtract(1, "years"));
-
-    // get & format current time
-    var currentTime = moment();
-    console.log("currentTime = " + moment(currentTime).format("hh:mm"));
-
-    // calculate time difference
-    var arrivalCalc = moment().diff(moment(arrivalConvert), "minutes");
-    console.log("arrivalCalc = " + moment().diff(moment(arrivalConvert), "minutes"));
-
-    // time remainder
-    var timeRem = arrivalCalc % frequency;
-    console.log("timeRem = " + (arrivalCalc % frequency));
-
-    // minutes until arrival
-    var arrival = frequency - timeRem;
-    console.log("arrival = " + (frequency - timeRem));
-
     // push to firebase
     firebase.database().ref().push({
         name: name,
-        destination: destination,
-        arrival: arrival,
-        departure: departure,
-        frequency: frequency
+        guestEmail: guestEmail,
+        message: message,
     });
 
     $("form")[0].reset();
 
-});
-
-// append schedule to table
-firebase.database().ref().on("child_added", function(snapshot){
-    console.log(snapshot.val());
-    $("#sched").append("<tr><td>" 
-        + snapshot.val().name 
-        + "</td><td>" 
-        + snapshot.val().destination 
-        + "</td><td>" 
-        + snapshot.val().arrival + " min(s)"
-        + "</td><td>" 
-        + snapshot.val().departure 
-        + "</td><td>" 
-        + snapshot.val().frequency 
-        + "</td></tr>");
-    }, function (errorObject) {
-        console.log("Errors handled: " + errorObject.code);
 });
 
